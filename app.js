575e41b80c0a1cdecfd5cb3c2513f8b3fd63ed44
Nangineni33 <123132843+Nangineni33@users.noreply.github.com>
2023-04-05 22:13:16 -0500

    commited

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catRouter = require('./routes/cat');
+var boardRouter = require('./routes/board');
+var selectRouter=require('./routes/selector')
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/cat', catRouter);
+app.use('/board',boardRouter);
+app.use('/selector',selectRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
